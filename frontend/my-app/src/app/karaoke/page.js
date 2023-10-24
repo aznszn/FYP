7a2033c97b03bbac6cf287d96bbb3c4a347bb226
NoumanAmir657 <noumanamir453@gmail.com>
2023-10-24 22:28:27 +0500

    frontend init

---
+++ frontend/my-app/src/app/karaoke/page.js
@@ -0,0 +1,11 @@
+import styles from './page.module.css';
+
+const Karaoke = () => {
+    return(
+        <div className={styles.centerX}>
+            Karaoke
+        </div>
+    )
+}
+
+export default Karaoke;
---
+++ frontend/my-app/src/app/page.js
@@ -0,0 +1,11 @@
+import styles from './page.module.css';
+
+const Home = () => {
+  return(
+    <div className={styles.centerX}>
+      Mouseeki
+    </div>
+  )
+}
+
+export default Home;

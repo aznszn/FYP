40c0b3fc92f50e68805a62424b43fe1097dec60f
NoumanAmir657 <noumanamir453@gmail.com>
2023-10-25 00:29:28 +0500

    backend init and frontend and backend connected

--- frontend/my-app/src/app/page.js
+++ frontend/my-app/src/app/page.js
@@ -1,11 +1,23 @@
-import styles from './page.module.css';
+// pages/index.js
+'use client';
+import React, { useEffect, useState } from 'react';
 
-const Home = () => {
-  return(
-    <div className={styles.centerX}>
-      Mouseeki
+function Home() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('http://127.0.0.1:8000/api/home/')
+      .then((response) => response.json())
+      .then((data) => setMessage(data.message))
+      .catch((error) => console.error(error));
+  }, []);
+
+  return (
+    <div>
+      <h1>Hello, Next.js!</h1>
+      <p>Message from the API: {message}</p>
     </div>
-  )
+  );
 }
 
 export default Home;
